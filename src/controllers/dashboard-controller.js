ffdf69616b1dee9ad808adbc46fd401d2f3f7b0a
OpinionatedHeron <leamoy@gmail.com>
2025-03-22 15:15:39 +0000

    Updating basic views - folders and locations

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,7 +1,24 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
-    index: {
-        handler: async function (request,h) {
-            return h.view("main");
-        },
+  index: {
+    handler: async function (request, h) {
+      const folders = await db.folderStore.getAllFolders();
+      const viewData = {
+        title: "PlaceMark Dashboard",
+        folders: folders,
+      };
+      return h.view("dashboard-view", viewData);
     },
+  },
+
+  addFolder: {
+    handler: async function (request, h) {
+      const newFolder = {
+        title: request.payload.title,
+      };
+      await db.folderStore.addFolder(newFolder);
+      return h.redirect("/dashboard");
+    },
+  },
 };
