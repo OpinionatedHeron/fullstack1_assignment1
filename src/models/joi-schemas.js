cdeb28acac02b063875b9e8d807b3b214c3d9f46
OpinionatedHeron <leamoy@gmail.com>
2025-03-23 17:44:30 +0000

    Adding Folder and Location API Info

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -1,32 +1,56 @@
 import Joi from "joi";
 
-export const IdSpec = Joi.alternatives().try(Joi.string(), Joi.object()).description("a valid ID");
+export const IdSpec = Joi.alternatives()
+  .try(Joi.string(), Joi.object())
+  .description("a valid ID");
 
-export const UserSpec = Joi.object()
+export const UserCredentialsSpec = Joi.object()
   .keys({
-    firstName: Joi.string().example("Riz").required(),
-    lastName: Joi.string().example("Gukgak").required(),
     email: Joi.string().email().example("riz@email.com").required(),
-    username: Joi.string().example("Shadow").required(),
     password: Joi.string().example("theball").required(),
-    _id: IdSpec,
-    __v: Joi.number()
   })
-  .label("UserDetails");
+  .label("UserCredentials");
 
-export const UserArray = Joi.array().items(UserSpec).label("UserArray");
+export const UserSpec = UserCredentialsSpec.keys({
+  firstName: Joi.string().example("Riz").required(),
+  lastName: Joi.string().example("Gukgak").required(),
+  username: Joi.string().example("Shadow").required(),
+}).label("UserDetails");
 
-export const UserCredentialsSpec = {
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-};
+export const UserSpecPlus = UserSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("UserDetailsPlus");
 
-export const LocationSpec = {
-  title: Joi.string().required(),
-  category: Joi.string().required(),
-  description: Joi.string().allow("").optional(),
-};
+export const UserArray = Joi.array().items(UserSpecPlus).label("UserArray");
 
-export const FolderSpec = {
-  title: Joi.string().required(),
-};
+export const LocationSpec = Joi.object()
+  .keys({
+    title: Joi.string().required().example("Croke Park"),
+    category: Joi.string().required().example("Venue"),
+    description: Joi.string().allow("").optional().example("A Gaelic games stadium."),
+    folderid: IdSpec,
+  })
+  .label("Location");
+
+export const LocationSpecPlus = LocationSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("LocationPlus");
+
+export const LocationArraySpec = Joi.array().items(LocationSpecPlus).label("LocationArray");
+
+export const FolderSpec = Joi.object()
+  .keys({
+    title: Joi.string().required().example("Kilkenny Faves"),
+    userid: IdSpec,
+    locations: LocationArraySpec,
+  })
+  .label("Folder");
+
+export const FolderSpecPlus = FolderSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("FolderPlus");
+
+export const FolderArraySpec = Joi.array().items(FolderSpecPlus).label("FolderArray");
