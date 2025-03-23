84859757ea800889faafecf5e42ce638134fe543
OpinionatedHeron <leamoy@gmail.com>
2025-03-23 16:24:22 +0000

    User API documentation

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -1,12 +1,16 @@
 import Joi from "joi";
 
-export const UserSpec = {
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  email: Joi.string().email().required(),
-  username: Joi.string().required(),
-  password: Joi.string().required(),
-};
+export const UserSpec = Joi.object()
+  .keys({
+    firstName: Joi.string().example("Riz").required(),
+    lastName: Joi.string().example("Gukgak").required(),
+    email: Joi.string().email().example("riz@email.com").required(),
+    username: Joi.string().example("Shadow").required(),
+    password: Joi.string().example("theball").required(),
+  })
+  .label("UserDetails");
+
+export const UserArray = Joi.array().items(UserSpec).label("UserArray");
 
 export const UserCredentialsSpec = {
   email: Joi.string().email().required(),
